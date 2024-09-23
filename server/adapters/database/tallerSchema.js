20a2af99297ba3520865f4668946c130edff3795
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-09-23 15:45:30 -0500

    feat: :sparkles: Se agrega schema para talleres (mongoose)

---
+++ server/adapters/database/tallerSchema.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const TallerSchema = mongoose.Schema({
+    nombre: {
+      type: String,
+      required: true,
+    },
+    descripcion: {
+      type: String,
+      required: true,
+    },
+    modalidad: {
+      type: String,
+      enum: ['presencial', 'virtual'],
+      required: true,
+    },
+    fechaInicio: {
+      type: Date,
+      required: true,
+    },
+    fechaFin: {
+      type: Date,
+      required: true,
+    },
+    duracion: {
+      type: String,
+      required: true,
+    },
+    materialesProporcionados: [{
+      type: String,
+    }],
+    materialesRequeridos: [{
+      type: String,
+    }],
+    documental: {
+      type: String,
+      default: '',
+    },
+    artesanoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Usuario',
+      required: true,
+    },
+  }, { timestamps: true });
+  
+  module.exports = mongoose.model('Taller', TallerSchema);
+  
