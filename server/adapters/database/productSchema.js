99fb8bc128fcfbce7375490b634218b6f3d3bee1
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-09-23 15:45:20 -0500

    feat: :sparkles: Se agrega schema para productos (mongoose)

---
+++ server/adapters/database/productSchema.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ProductoSchema =  mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  precio: {
+    type: mongoose.Types.Decimal128,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+  fotos: [{
+    type: String,
+  }],
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  artesanoId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Producto', ProductoSchema);
