a276a69024b33804a41a81c95116c82da7716fae
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-09-24 15:15:38 -0500

    feat: :sparkles: verificacion de token para el manejo de las funciones

--- server/application/routes/productRoute.js
+++ server/application/routes/productRoute.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const ProductController = require("../controllers/productController")
 const ProductValidator = require("../validator/productValidator")
+const {auth} = require('../middlewares/authenticateToken')
 
 const router  = express.Router()
 const productController = new ProductController();
 const productValidator = new ProductValidator();
 
-router.get("/searchAll", productValidator.validateProductDataEmpty(), (req, res) => productController.getProducts(req, res))
-router.post("/", productValidator.validateProductData(), (req, res) => productController.createProduct(req, res))
-router.put('/:id', productValidator.validateProductUpdateDataByID(), (req, res) => productController.updateProduct(req, res));
-router.delete('/:id', productValidator.validateProductId(), (req, res) => productController.deleteProduct(req, res));
+router.get("/searchAll", auth, productValidator.validateProductDataEmpty(), (req, res) => productController.getProducts(req, res))
+router.post("/", auth, productValidator.validateProductData(), (req, res) => productController.createProduct(req, res))
+router.put('/:id', auth, productValidator.validateProductUpdateDataByID(), (req, res) => productController.updateProduct(req, res));
+router.delete('/:id', auth, productValidator.validateProductId(), (req, res) => productController.deleteProduct(req, res));
 
 module.exports = router;
--- server/application/routes/userRoute.js
+++ server/application/routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const {auth} = require('../middlewares/authenticateToken')
 
 const UserController = require("../controllers/userController")
 const UserValidator = require("../validator/userValidator")
@@ -8,10 +9,10 @@ const router  = express.Router()
 const userController = new UserController();
 const userValidator = new UserValidator();
 
-router.get("/:id", userValidator.validateUserDataEmpty(), (req, res) => userController.getUser(req, res))
+router.get("/:id", auth, userValidator.validateUserDataEmpty(), (req, res) => userController.getUser(req, res))
 router.post("/", userValidator.validateUserData(), (req, res) => userController.createUser(req, res))
 router.post('/login', cookieParser(), userValidator.validateUserLogin(), (req, res) => userController.verifyUserCookies(req, res))
-router.put('/:id', userValidator.validateUserUpdateDataById(), (req, res) => userController.updateUser(req, res));
-router.delete('/:id', userValidator.validateUserId(), (req, res) => userController.deleteUser(req, res));
+router.put('/:id', auth, userValidator.validateUserUpdateDataById(), (req, res) => userController.updateUser(req, res));
+router.delete('/:id', auth, userValidator.validateUserId(), (req, res) => userController.deleteUser(req, res));
 
 module.exports = router;
