d93d0381444a7c62748684be717baabb7ae35c65
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-09-26 14:10:06 -0500

    feat: :recycle: cambios en el package.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app'
+import App from './App'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
