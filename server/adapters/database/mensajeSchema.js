ae0c9663304c2660bce33e9a5b904da818e69b94
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-09-30 16:22:52 -0500

    refactor: :sparkles: Cambio en el Schema de mensaje para implementar el chat

--- server/adapters/database/mensajeSchema.js
+++ server/adapters/database/mensajeSchema.js
@@ -1,25 +1,10 @@
 const mongoose = require("mongoose");
 
 const MensajeSchema = mongoose.Schema({
-    remitenteId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'usuario',
-      required: true,
-    },
-    receptorId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'usuario',
-      required: true,
-    },
-    contenido: {
-      type: String,
-      required: true,
-    },
-    fecha: {
-      type: Date,
-      default: Date.now,
-    },
+  message: String,
+  user: String,
+  createdAt: { type: Date, default: Date.now },
   }, { timestamps: true });
   
-  module.exports = mongoose.model('mensaje', MensajeSchema, 'mensaje');
+  module.exports = mongoose.model('mensaje', MensajeSchema);
   
