47f7c8eb4789d208877dd9d91355a3681e3fc171
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-10-02 17:38:17 -0500

    feat: :sparkles: tiendas o artesanos view is already done

--- client/src/pages/Tiendas.jsx
+++ client/src/pages/Tiendas.jsx
@@ -1,44 +1,48 @@
-import React from "react";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Tiendas = () => {
-    const artworks = [
-        { title: "Arte Abedail Aller Escalante", location: "Cusco", imageSrc: "/public/tiendas/instance.png" },
-        { title: "Asoc. de artesanos Tinkuy", location: "Huánuco", imageSrc: "/public/tiendas/instance.png" },
-        { title: "Retablos Jesús Urbano", location: "Ayacucho", imageSrc: "/public/tiendas/instance.png" },
-        { title: "Taller Awaq Ayllus", location: "Ayacucho", imageSrc: "/public/tiendas/instance.png" },
-        { title: "Retablos Jesús Urbano", location: "Ayacucho", imageSrc: "/public/tiendas/instance.png" },
-        { title: "Taller Awaq Ayllus", location: "Ayacucho", imageSrc: "/public/tiendas/instance.png" },      
-    ];
+    const [tiendas, setTiendas] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/users/getAllUsersTypeArtesano')
+            .then(response => response.json())
+            .then(data => setTiendas(data))
+            .catch(error => console.error('Error fetching workshops:', error));
+    }, []);
+
+    console.log(tiendas);
 
     return (
         <>
             <Header />
             <div className="w-[100vw]">
-            <div className="flex items-center justify-between mx-auto w-full mt-4 mb-6">
+                <div className="flex items-center justify-between mx-auto w-full mt-4 mb-6">
                     <img src="/public/tiendas/Rectangle 35.png" alt="Ícono" className='mr-2 w-9 h-12' />
                     <div className="flex flex-col flex-grow px-4">
                         <p className="font-semibold">Talleres y tiendas artesanales</p>
                         <p className="text-sm text-[#A3A3A3]">Tiendas de artesanías de Santander Colombia</p>
                     </div>
                     <img src="/public/tiendas/graph.svg" alt="Ícono" className='ml-2 mr-3 w-9 h-10' />
-
                 </div>
-                </div>
+            </div>
             <div className='w-full px-4'>
-    
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                    {artworks.map((artwork, index) => (
+                    {tiendas.map((tienda, index) => (
                         <div key={index} className="w-40 mb-4">
                             <div className="bg-black text-white p-2 rounded-t-lg h-15">
-                                <p className="text-xs font-bold">{artwork.title}</p>
-                                <h2 className="text-xs">{artwork.location}</h2>
+                                <p className="text-xs font-bold">{tienda.nombre}</p>
+                                <h2 className="text-xs">{tienda.direccion}</h2>
                             </div>
-                            <div className="h-25 overflow-hidden rounded-b-lg">
-                                <img src={artwork.imageSrc} alt={artwork.title} className="w-full h-full object-cover" />
-                            </div>
-                        </div>
+                            <div className="h-25 overflow-hidden rounded-b-lg  min-h-[120px] max-h-[120px] flex items-center justify-center">
+    <img 
+        src={tienda.fotoPerfil} 
+        alt={tienda.title} 
+        className="max-w-[100%] max-h-[100%] object-contain" 
+    />
+</div>
+                </div>
                     ))}
                 </div>
             </div>
@@ -48,4 +52,3 @@ const Tiendas = () => {
 };
 
 export default Tiendas;
-
