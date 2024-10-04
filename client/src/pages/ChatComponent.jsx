bfa7ca8f4146dcab8539c3d5cffaf02deeeb3cab
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-10-04 00:47:16 -0500

    fix: :bug: correccion de estilos en la respuesta del server

--- client/src/pages/ChatComponent.jsx
+++ client/src/pages/ChatComponent.jsx
@@ -132,13 +132,12 @@ const Chat = () => {
             <div ref={chatContainerRef} className="flex-1 overflow-y-auto p-4 space-y-4 relative px-[20px] py-[30px] min-h-[75%]">
                 {messages.map((message, index) => (
                     <div key={index} 
-                         className={`flex ${message.username === 'Soporte' ? 'justify-start' : 'justify-end'}`}>
+                         className={`flex ${message.username === 'Soporte'||message.transmitter === 'server' ? 'justify-start' : 'justify-end'}`}>
                         <div className={`max-w-xs lg:max-w-md px-4 py-2 rounded-lg ${
-                            message.username === 'Soporte' 
+                            message.username === 'Soporte' ||message.transmitter === 'server'
                                 ? 'bg-[#3D3D3D] text-white' 
                                 : 'bg-[#D9D9D9] text-black'
                         }`}>
-                            {console.log(message)}
                             <p className="text-sm">{message.text || message.texto}</p>
                             <p className="text-xs mt-1 opacity-70">
                                 {message.transmitter === 'server' ? 'Soporte' : message.username}
